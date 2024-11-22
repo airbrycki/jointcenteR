7e4ce45a8e23a8659ff90320b40da47acae65907
airbrycki <63924344+airbrycki@users.noreply.github.com>
2024-11-21 19:45:53 -0500

    Update data.R

--- R/data.R
+++ R/data.R
@@ -1,9 +1,11 @@
 #' random sample of 2022 American Community Survey PUMS
 #'
 #' Household data for a random sample of 300 observations taken from the 2022 American Community Survey Public Use Microdata 1-Year Estimates.
+#' @docType data
 #'
+#' @usage data(acs22)
 #' @source U.S. Census Bureau, 2022 American Community Survey 1-Year PUMS.
-#' @format A data frame with columns:
+#' @format A data frame with111 columns:
 #' \describe{
 #'  \item{serialno}{Unique household idnetifier}
 #'  \item{region}{Region of the country the household lives in}
