6b01b4a2bbf0cb6f4099340c5469733e80b4c20f
airbrycki <whitney_airgood-obrycki@harvard.edu>
2025-01-06 14:17:26 -0500

    updated dependency

--- R/get_permits.R
+++ R/get_permits.R
@@ -3,28 +3,28 @@
 #' Cleans census URLs to directly download and format annual county or state permit
 #' datasets
 #'
-#' @param geography Select the geography to pull. Valid inputs are "state" or 
-#'                  "county" 
-#' 
+#' @param geography Select the geography to pull. Valid inputs are "state" or
+#'                  "county"
+#'
 #' @param year The year to extract, starting with 1980 for states and 1990 for
 #'             counties.
 #'
 #' @return A tibble with 31 columns.
 #'
-#' @note Adapted from Chris Goodman: 
+#' @note Adapted from Chris Goodman:
 #'       "https://gist.github.com/cbgoodman/8b78153e93148cdff370e3c6f3629ade.js"
 #'       Can extract multiple years at once using purrr::map (see examples).
 #'
 #' @examples
 #' # pull state permits for 2023 and select unit variables
 #' sp23 <- get_permits("state", 2023)
-#' sp23 |> 
+#' sp23 |>
 #'   select(year, region, state_name, starts_with("units_") & ends_with("unit"))
 #'
-#' # pull multiple years at once and bind into long df 
+#' # pull multiple years at once and bind into long df
 #' sp_list <- map(2003:2023, get_permits, geography = "state")
 #' sp <- bind_rows(sp_list)
-#' 
+#'
 #' @export
 get_permits <- function(geography, year) {
   if (geography != "state" & geography != "county") {
@@ -32,26 +32,26 @@ get_permits <- function(geography, year) {
   }
   else if (geography == "state") {
     url_loc <- "www2.census.gov/econ/bps/State/"
-    
+
     files <- rvest::read_html("https://www2.census.gov/econ/bps/State/") |>
       rvest::html_elements("a") |>
       rvest::html_text2()
-    
+
     ### Just take files containing  `coYYYYa``
-    fname_stubs <- files[str_detect(files, "(st)\\d{4}(:?a)")]
+    fname_stubs <- files[stringr::str_detect(files, "(st)\\d{4}(:?a)")]
     fname_stubs <- fname_stubs[grepl(year, fname_stubs) == TRUE]
-    
+
     ### Add year
     fname_year <- substr(fname_stubs, start = 3, stop = 6)
-    
+
     ### Clean up the URLs
     fnames <- paste0("https://", url_loc, fname_stubs)
-    fnames <- set_names(fnames, fname_year)
-    
+    fnames <- rlang::set_names(fnames, fname_year)
+
     ### Import
     cpb <- fnames |>
-      purrr::map(~ read_csv(., skip = 1)) |>
-      list_rbind(names_to = "year") |>
+      purrr::map(~ readr::read_csv(., skip = 1)) |>
+      purrr::list_rbind(names_to = "year") |>
       dplyr::rename(
         survey = Date,
         state_fips = State,
@@ -83,31 +83,31 @@ get_permits <- function(geography, year) {
         units_5unit_rep = "Units...28",
         val_5unit_rep = "Value...29"
       )
-  } 
+  }
   else {
     url_loc <- "www2.census.gov/econ/bps/County/"
-    
+
     files <- rvest::read_html("https://www2.census.gov/econ/bps/County/") |>
       rvest::html_elements("a") |>
       rvest::html_text2()
-    
+
     ### Just take files containing  `coYYYYa``
-    fname_stubs <- files[str_detect(files, "(co)\\d{4}(:?a)")]
+    fname_stubs <- files[stringr::str_detect(files, "(co)\\d{4}(:?a)")]
     # remove 2022 update file
     fname_stubs <- fname_stubs[-34]
     fname_stubs <- fname_stubs[grepl(year, fname_stubs) == TRUE]
 
     ### Add year
     fname_year <- substr(fname_stubs, start = 3, stop = 6)
-    
+
     ### Clean up the URLs
     fnames <- paste0("https://", url_loc, fname_stubs)
-    fnames <- set_names(fnames, fname_year)
-    
+    fnames <- rlang::set_names(fnames, fname_year)
+
     ### Import
     cpb <- fnames |>
-      purrr::map(~ read_csv(., skip = 1)) |>
-      list_rbind(names_to = "year") |>
+      purrr::map(~ readr::read_csv(., skip = 1)) |>
+      purrr::list_rbind(names_to = "year") |>
       dplyr::rename(
         survey = Date,
         state_fips = State,
