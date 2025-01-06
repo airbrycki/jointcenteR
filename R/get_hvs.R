4d5bfda1821e3b310e4534485396199c5fe8a5d9
airbrycki <whitney_airgood-obrycki@harvard.edu>
2025-01-06 13:59:01 -0500

    fixed dependency

--- R/get_hvs.R
+++ R/get_hvs.R
@@ -5,12 +5,12 @@
 #' @return A tibble of HVS variables for each quarter from 2Q2001 through most
 #'         recent available.
 #'
-#' @note Adapted from Len Kiefer: 
+#' @note Adapted from Len Kiefer:
 #'       http://lenkiefer.com/2017/09/18/a-tidyquant-um-of-solace/
 #'
 #' @examples
 #' hvs <- get_hvs()
-#' 
+#'
 #' @export
 get_hvs <- function() {
 
@@ -49,25 +49,25 @@ get_hvs <- function() {
                        'rental_vacancy_rate',
                        'homeowner_vacancy_rate'
   )
-  
+
   # Create a lookup dataset
   variable_table<-data.frame(symbol=tickers,var=variable_labels)
-  
+
   # pull data using tidyquant
-  df <- tidyquant::tq_get(tickers, get = "economic.data", from = "2001-04-01") |> 
-          dplyr::rename(value = price) |> 
-          dplyr::left_join(variable_table, by = "symbol") |> 
-          dplyr::arrange(var, date) |> 
-          dplyr::select(date, var, value) |> 
-          dplyr::mutate(value_4q_avg = if_else(date < 2002-01-01,
+  df <- tidyquant::tq_get(tickers, get = "economic.data", from = "2001-04-01") |>
+          dplyr::rename(value = price) |>
+          dplyr::left_join(variable_table, by = "symbol") |>
+          dplyr::arrange(var, date) |>
+          dplyr::select(date, var, value) |>
+          dplyr::mutate(value_4q_avg = dplyr::if_else(date < 2002-01-01,
                                                NA_integer_,
-                                               pracma::movavg(value, 4))) |> 
-          tidyr::pivot_wider(names_from = var, values_from = c("value", "value_4q_avg")) |> 
-          dplyr::mutate(value_homeownership_rate = 
+                                               pracma::movavg(value, 4))) |>
+          tidyr::pivot_wider(names_from = var, values_from = c("value", "value_4q_avg")) |>
+          dplyr::mutate(value_homeownership_rate =
                             value_owner_occupied / value_occupied * 100,
-                        value_4q_avg_homeownership_rate = 
+                        value_4q_avg_homeownership_rate =
                             value_4q_avg_owner_occupied / value_4q_avg_occupied * 100)
-  
+
   return(df)
 
 }
