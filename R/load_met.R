d0bac2f219a6058d6b7af101564131391773666d
airbrycki <whitney_airgood-obrycki@harvard.edu>
2025-06-23 17:17:01 -0400

    updating to account for new 2023 cbsa boundaries

--- R/load_met.R
+++ R/load_met.R
@@ -22,34 +22,74 @@
 #'
 #' @export
 load_met <- function(yr, path = acspath) {
-  #metro cbsas to merge on
-  cbsapop <- tidycensus::get_acs(geography = "cbsa",
-                                 variables = "B01003_001",
-                                 year=2022) |>
-    dplyr::select(-moe, -variable) |>
-    dplyr::rename(cbsapop_5yr = estimate) |>
-    dplyr::filter(!grepl(", PR", NAME)) |>
-    dplyr::mutate(cbsa20 = as.numeric(GEOID),
-                  poprank_22_5yr = dplyr::min_rank(dplyr::desc(cbsapop_5yr))) |>
-    dplyr::rename(CBSA_name_5yr = NAME) |>
-    dplyr::select(-GEOID)
 
-  inputfile_met <- paste0("ACS_", yr, "_hhplus_metro.csv")
+    inputfile_met <- paste0("ACS_", yr, "_hhplus_metro.csv")
   df <- data.table::fread(file.path(path, inputfile_met))
 
-  df <- df |>
-    dplyr::left_join(cbsapop, by = "cbsa20") |>
-    dplyr::mutate(metro = dplyr::if_else(grepl("Metro", CBSA_name_5yr), 1, 0),
-                 micro = dplyr::if_else(grepl("Micro", CBSA_name_5yr), 1, 0),
-                 rural = dplyr::if_else(is.na(CBSA_name_5yr), 1, 0),
-                 metsize_22_5yr = dplyr::case_when(metro==1 & cbsapop_5yr>=1000000 ~ "1 largestmet",
-                                                    metro==1 & cbsapop_5yr>=250000 & cbsapop_5yr<1000000 ~ "2 midmet",
-                                                    (metro==1 & cbsapop_5yr<250000) | micro==1 ~ "3 smallmet_micro",
-                                                    rural==1 ~ "4 rural"),
-                 metsize2_22_5yr = dplyr::case_when(metro==1 & poprank_22_5yr <= 100 ~ "1 top100",
-                                                     metro==1  ~ "2 other metro",
-                                                     micro==1 ~ "3 micro",
-                                                     rural==1 ~ "4 rural"))
+  if ("cbsa23" %in% names(df)) {
+
+    message("adding 2023 metro pop ranks and metro size variables")
+
+    #metro cbsas to merge on
+    cbsapop <- tidycensus::get_acs(geography = "cbsa",
+                                   variables = "B01003_001",
+                                   year=2023) |>
+      dplyr::select(-moe, -variable) |>
+      dplyr::rename(cbsapop_5yr = estimate) |>
+      dplyr::filter(!grepl(", PR", NAME)) |>
+      dplyr::mutate(cbsa23 = as.numeric(GEOID),
+                    poprank_23_5yr = dplyr::min_rank(dplyr::desc(cbsapop_5yr))) |>
+      dplyr::rename(CBSA_name_5yr = NAME) |>
+      dplyr::select(-GEOID)
+
+
+      df <- df |>
+        dplyr::left_join(cbsapop, by = "cbsa23") |>
+        dplyr::mutate(metro = dplyr::if_else(grepl("Metro", CBSA_name_5yr), 1, 0),
+                     micro = dplyr::if_else(grepl("Micro", CBSA_name_5yr), 1, 0),
+                     rural = dplyr::if_else(is.na(CBSA_name_5yr), 1, 0),
+                     metsize_23_5yr = dplyr::case_when(metro==1 & cbsapop_5yr>=1000000 ~ "1 largestmet",
+                                                        metro==1 & cbsapop_5yr>=250000 & cbsapop_5yr<1000000 ~ "2 midmet",
+                                                        (metro==1 & cbsapop_5yr<250000) | micro==1 ~ "3 smallmet_micro",
+                                                        rural==1 ~ "4 rural"),
+                     metsize2_23_5yr = dplyr::case_when(metro==1 & poprank_23_5yr <= 100 ~ "1 top100",
+                                                         metro==1  ~ "2 other metro",
+                                                         micro==1 ~ "3 micro",
+                                                         rural==1 ~ "4 rural"))
+  } else if ("cbsa20" %in% names(df)) {
+
+      message("adding 2022 metro pop ranks and metro size variables")
+
+      #metro cbsas to merge on
+      cbsapop <- tidycensus::get_acs(geography = "cbsa",
+                                     variables = "B01003_001",
+                                     year=2022) |>
+        dplyr::select(-moe, -variable) |>
+        dplyr::rename(cbsapop_5yr = estimate) |>
+        dplyr::filter(!grepl(", PR", NAME)) |>
+        dplyr::mutate(cbsa20 = as.numeric(GEOID),
+                      poprank_22_5yr = dplyr::min_rank(dplyr::desc(cbsapop_5yr))) |>
+        dplyr::rename(CBSA_name_5yr = NAME) |>
+        dplyr::select(-GEOID)
+
+
+      df <- df |>
+        dplyr::left_join(cbsapop, by = "cbsa20") |>
+        dplyr::mutate(metro = dplyr::if_else(grepl("Metro", CBSA_name_5yr), 1, 0),
+                      micro = dplyr::if_else(grepl("Micro", CBSA_name_5yr), 1, 0),
+                      rural = dplyr::if_else(is.na(CBSA_name_5yr), 1, 0),
+                      metsize_22_5yr = dplyr::case_when(metro==1 & cbsapop_5yr>=1000000 ~ "1 largestmet",
+                                                        metro==1 & cbsapop_5yr>=250000 & cbsapop_5yr<1000000 ~ "2 midmet",
+                                                        (metro==1 & cbsapop_5yr<250000) | micro==1 ~ "3 smallmet_micro",
+                                                        rural==1 ~ "4 rural"),
+                      metsize2_22_5yr = dplyr::case_when(metro==1 & poprank_22_5yr <= 100 ~ "1 top100",
+                                                         metro==1  ~ "2 other metro",
+                                                         micro==1 ~ "3 micro",
+                                                         rural==1 ~ "4 rural"))
+  } else {
+    message("loading file but cannot add metro pop ranks and size variables")
+    df <- df
+  }
 
   dfyr <- paste0("met", yr)
   assign(dfyr, df, envir = .GlobalEnv)
