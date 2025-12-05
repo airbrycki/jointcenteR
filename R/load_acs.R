ce6d24a768f4d0fb3e418ff194405b00ad98b202
airbrycki <whitney_airgood-obrycki@harvard.edu>
2025-12-05 13:04:04 -0500

    simplified function to increase speed

--- R/load_acs.R
+++ R/load_acs.R
@@ -28,13 +28,6 @@
 #'
 #' @export
 load_acs <- function(year, path = acspath, type = "csv") {
-  # use tidycensus to get state names
-  fips_codes <- tidycensus::fips_codes |>
-    dplyr::mutate(st = as.numeric(state_code), StateName = state_name) |>
-    dplyr::select(st, StateName) |>
-    dplyr::distinct() |>
-    dplyr::filter(st <= 56)
-
   # load acs years-----
   ## .csv files-----
   if (type == "csv") {
@@ -43,6 +36,10 @@ load_acs <- function(year, path = acspath, type = "csv") {
     if (year >= 2015) {
       inputfile <- paste0("ACS_", year, "_hhplus.csv")
       df <- data.table::fread(file.path(path, inputfile))
+
+      if (year > 2022) {
+        df <- df |> dplyr::mutate(st = state)
+      }
 
       # multiyear file
     } else if (year >= 2001 & year < 2015) {
@@ -69,6 +66,10 @@ load_acs <- function(year, path = acspath, type = "csv") {
     if (year >= 2015) {
       inputfile <- paste0("ACS_", year, "_hhplus.dta")
       df <- haven::read_dta(file.path(path, inputfile))
+
+      if (year > 2022) {
+        df <- df |> dplyr::mutate(st = state)
+      }
 
       # multiyear file
     } else if (year >= 2001 & year < 2015) {
@@ -99,45 +100,15 @@ load_acs <- function(year, path = acspath, type = "csv") {
     df <- data.table::data.table(DBI::dbGetQuery(con, query))
     duckdb::dbDisconnect(con)
 
+    if (year > 2022) {
+      df <- df |> dplyr::mutate(st = state)
+    }
+
     ## any other file type
   } else {
     warning("file type not supported")
     stop()
   }
-
-  # add another agecat variable
-  df <- df |>
-    dplyr::mutate(
-      agecat4 = factor(
-        dplyr::case_when(
-          agep < 25 ~ 1,
-          agep >= 25 & agep < 55 ~ 2,
-          agep >= 55 & agep < 65 ~ 3,
-          agep >= 65 ~ 4
-        ),
-        levels = c(1:4),
-        labels = c("Under 25", "25-54", "55-64", "65+")
-      )
-    )
-
-  # add simplified hh_inccat varaible
-  if (year > 2022) {
-    df <- df |>
-      dplyr::mutate(st = state) |>
-      dplyr::mutate(
-        hh_inccat2 = factor(
-          dplyr::case_when(
-            hincp < 30000 ~ 1,
-            hincp >= 30000 & hincp < 75000 ~ 2,
-            hincp >= 75000 ~ 3
-          ),
-          levels = c(1:3),
-          labels = c("Under 30,000", "30,000-74,999", "75,000+")
-        )
-      )
-  }
-  df <- df |>
-    dplyr::left_join(fips_codes, by = "st")
 
   dfyr <- paste0("acs", year)
   assign(dfyr, df, envir = .GlobalEnv)
