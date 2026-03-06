e658f91f769309d5c10826048da1425461f07766
airbrycki <whitney_airgood-obrycki@harvard.edu>
2026-03-06 14:40:43 -0500

    removed tidyquant dependency

--- R/jointcenteR-package.R
+++ R/jointcenteR-package.R
@@ -5,7 +5,6 @@
 #' @importFrom Hmisc wtd.quantile
 #' @importFrom lubridate year
 #' @importFrom tidycensus get_acs
-#' @importFrom tidyquant tq_get
 #' @importFrom tidyr pivot_wider
 ## usethis namespace: end
 NULL
